02e2dac13074f698e0096f162086f9a10452c0ed
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-10-18 06:43:52 -0700

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# This code will enable the user holberton to login && open files without getting an err
+
+# Will increase hard limit for the user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Will increase soft limit for the user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
